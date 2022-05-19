08db72e296cb9d39ff636310f297f60a79b098be
victo <vicky454ph@gmail.com>
2022-05-18 23:48:17 -0500

    iniciando

---
+++ app/src/main/java/com/example/retrofitapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.retrofitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    //para consumir una appi, se necesita
+        //1.- agregar las implementaciones en Gradle
+        //2.- darle permiso en ManiFest
+        //3.- crear Data Class en remote
+
+    }
+}
---
+++ app/src/main/java/com/example/retrofitapp/remote/RetroFitBuilder.kt
@@ -0,0 +1,24 @@
+package com.example.retrofitapp.remote
+
+import android.telecom.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RetroFitBuilder {
+    @GET("pokemon/{id}")
+    fun getPokemonById(@Path("id") id:String): retrofit2.Call<PokemonEntry>
+
+    companion object{
+       private val BASE_URL = "https://pokeapi.co/api/v2/pokemon/27"
+
+        fun create(): RetroFitBuilder{
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+                    return retrofit.create(RetroFitBuilder::class.java)
+        }
+    }
+}
